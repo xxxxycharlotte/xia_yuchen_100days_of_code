c9c413d7ff970191db8d8c766c24d2e35a743b7e
Yuchen Xia <39134500+xxxxycharlotte@users.noreply.github.com>
2022-01-19 02:15:37 +0800

    1.19 upload

---
+++ ball/sketch.js
@@ -0,0 +1,23 @@
+
+var x=200;
+var y=200;
+var Vx=2;
+var Vy=3;
+ 
+function setup() { 
+  createCanvas(400, 400);
+} 
+ 
+function draw() { 
+  background(220);
+  ellipse(x,y,20,20);
+  x+=Vx;
+  y+=Vy;
+	if(x>width||x<0){
+		Vx*=-1;
+  }
+  if(y>height||y<0){
+		Vy*=-1;
+  }
+}
+
